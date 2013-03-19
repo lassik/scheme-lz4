726bbdc790a218d2746112d8ef94e5e7ab1b12c6
Christian Kellermann <ckeen@pestilenz.org>
2013-03-19 12:10:15 +0100

    Make lz4 more neighbourly by raising '(exn ffi lz4) conditions instead of calling assert and error.

--- lz4.scm
+++ lz4.scm
@@ -10,6 +10,12 @@
 (foreign-declare "#include \"lz4.h\"")
 (foreign-declare "#include \"lz4hc.h\"")
 
+(define (lz4-condition loc msg . args)
+  (make-composite-condition
+   (make-property-condition 'exn 'arguments args 'location loc 'message msg)
+   (make-property-condition 'ffi)
+   (make-property-condition 'lz4)))
+
 (define foreign-compress
   (foreign-lambda int "LZ4_compress" s8vector s8vector integer))
 
@@ -20,25 +26,28 @@
   (foreign-lambda int "LZ4_compressHC" s8vector s8vector integer))
 
 (define (compress in-buf)
-  (assert (s8vector? in-buf) "Argument to compress must be a srfi-4 s8vector, got " in-buf)
+  (unless (s8vector? in-buf)
+    (signal (lz4-condition 'compress "Argument to compress must be a srfi-4 s8vector, got" in-buf)))
   (let* ((len (s8vector-length in-buf))
          (out-buf (make-s8vector (add1 len) 0)) ;; plus 1 for the header byte
          (bytes-written (foreign-compress in-buf out-buf len)))
-    (unless (< 0 bytes-written) (error "error returned from compress procedure"))
+    (unless (< 0 bytes-written) (signal (lz4-condition 'compress "error returned from compress procedure")))
     (subs8vector out-buf 0  bytes-written)))
 
 (define (compress-hc in-buf)
-  (assert (s8vector? in-buf) "Argument to compress must be a srfi-4 s8vector, got " in-buf)
+  (unless (s8vector? in-buf)
+    (signal (lz4-condition 'compress-hc "Argument to compress must be a srfi-4 s8vector, got" in-buf)))
   (let* ((len (s8vector-length in-buf))
          (out-buf (make-s8vector (add1 len) 0))
          (bytes-written (foreign-compress-hc in-buf out-buf len)))
-    (unless (< 0 bytes-written) (error "error returned from compress procedure"))
+    (unless (< 0 bytes-written) (signal (lz4-condition 'compress-hc "error returned from compress procedure")))
     (subs8vector out-buf 0  bytes-written)))
 
 (define (uncompress in-buf)
-  (assert (s8vector? in-buf) "Argument to uncompress must be a srfi-4 s8vector, got " in-buf)
+  (unless (s8vector? in-buf)
+    (signal (lz4-condition 'uncompress "Argument to uncompress must be a srfi-4 s8vector, got" in-buf)))
   (let* ((len (sub1 (s8vector-length in-buf)))
          (out-buf (make-s8vector len 0))
          (bytes-written (foreign-uncompress in-buf out-buf len)))
-    (unless (< 0 bytes-written) (error "error returned from uncompress procedure"))
+    (unless (< 0 bytes-written) (signal (lz4-condition 'uncompress "error returned from compress procedure")))
     (subs8vector out-buf 0 len))))
